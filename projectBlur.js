a083a1cd851f5db1b3429037898a03c517902b3c
Brody Jackson <brodyjackson1@gmail.com>
2017-07-31 09:21:26 -0600

    initial live site

---
+++ projectBlur.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const projectClasses = getElementsByClassName("project");
+
+const addInfo = function(){
+    alert("hello");
+}
+
+for (let i = 0; i < projectClasses.length; i++){
+    projectsClasses[i].addEventListener('mouseover', addInfo);
+}
